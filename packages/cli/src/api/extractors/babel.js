7ae8b5cb77890c5683337fcae585a9c1d88bc820
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2018-07-29 11:13:28 +0200

    feat: merge plugins with macros

--- packages/cli/src/api/extractors/babel.js
+++ packages/cli/src/api/extractors/babel.js
@@ -1,8 +1,6 @@
 // @flow
 import { transformFileSync } from "babel-core"
 
-import linguiTransformJs from "@lingui/babel-plugin-transform-js"
-import linguiTransformReact from "@lingui/babel-plugin-transform-react"
 import linguiExtractMessages from "@lingui/babel-plugin-extract-messages"
 
 import type { ExtractorType } from "./types"
@@ -18,16 +16,7 @@ const extractor: ExtractorType = {
     const plugins = options.plugins || []
     transformFileSync(filename, {
       ...options,
-      plugins: [
-        // Plugins run before presets, so we need to import transform-plugins
-        // here until we have a better way to run extract-messages plugin
-        // *after* all plugins/presets.
-        // Transform plugins are idempotent, so they can run twice.
-        linguiTransformJs,
-        linguiTransformReact,
-        [linguiExtractMessages, { localeDir }],
-        ...plugins
-      ]
+      plugins: [[linguiExtractMessages, { localeDir }], ...plugins]
     })
   }
 }
